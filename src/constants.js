ae1ca47eb8e13b6abe6168d907a7bef222452a53
Kazi Jawadul Islam Jishan <87614554+Jobaduwul@users.noreply.github.com>
2024-05-06 12:20:52 +0600

    update with my contents

--- src/constants.js
+++ src/constants.js
@@ -1,23 +1,20 @@
 export const scaleFactor = 4;
 
 export const dialogueData = {
-  pc: `This is my PC. I work mostly in JavaScript/TypeScript these days.
-    I've made a couple of games in that language. I also like Golang and Python. Anyway regardless of the language, I just like programming.
-    Here is my <a href="https://github.com/jslegenddev" target="_blank">Github</a>!`,
-  "cs-degree": `This is my CS degree. I hung it on the wall because I'm proud of it. It was a very theoretical degree but I think it gave me a good foundation.`,
+  pc: `This is my PC. I work mostly in Unity 3D Engine, Unreal Engine and Godot Engine.
+    I've made a couple of games in that language. I also like C# and C++. Anyway regardless of the language, I just love to make and play games.
+    Here is my <a href="https://github.com/Jobaduwul" target="_blank">Github</a> and <a href="https://www.linkedin.com/in/jobaduwul/" target="_blank">Linkedin</a>`,
+  "cs-degree": `This is my academics certificates. I hung it on the wall because I'm proud of it. School: SFX Greenherald Int'l School (2006 - 2020). University: Islamic University of Technology (2021 - present).`,
   "sofa-table": `That's my sofa. I like to relax here and watch YouTube. 
-  I also make game programming tutorials on YouTube. Go sub to <a href="https://youtube.com/@jslegenddev" target="_blank">my channel</a>! (If you like the content)
-  You'll learn how I built this portfolio you're currently playing through!`,
-  tv: `That's my TV. I've been watching tech youtubers a lot recently like :
-   <a href="https://www.youtube.com/@ThePrimeTimeagen" target="_blank">Theprimeagen</a>, <a href="https://www.youtube.com/@t3dotgg" target="_blank">Theo - t3.gg</a>,
-  <a href="https://www.youtube.com/@PirateSoftware" target="_blank">PirateSoftware</a> (sometimes) and <a href="https://www.youtube.com/@MelkeyDev" target="_blank">Melkey</a>!`,
+  I also love to watch videos on YouTube. (If you like the content)
+  You'll learn how I built this portfolio you're currently playing through<a href="https://youtube.com/@jslegenddev" target="_blank">here</a>!`,
+  tv: `That's my TV. I've been watching tech youtubers a lot recently`,
   bed: `This where I sleep. Great ideas comes when I'm lying on my bed. When an idea strikes, I often have to write it down or else I won't be able to sleep because my mental energy is consumed by it.`,
-  resume: `This is my desk and on it is my resume. <a href="https://github.com/JSLegendDev/Resume/blob/main/JSLegend%20Resume-1.pdf" target="_blank">Check it out?</a>
-  Contact me at jslegend@protonmail.com if you have any interesting job opportunities!`,
+  resume: `This is my desk and on it is my resume. <a href="https://www.linkedin.com/in/jobaduwul/overlay/1711832834914/single-media-viewer/?profileId=ACoAADbAR8IB69YrdfKQ6pAJ9AmHSgUCE6XFKCQ" target="_blank">Check it out?</a>
+  Contact me at kazijawadulislam123@gmail.com if you have any interesting job opportunities!`,
   projects: `Info about this portfolio : It's made with the Kaboom.js library which is a library for making games in JavaScript.
-  Text is rendered with HTML/CSS. So the textbox you're currently reading is not rendered within canvas. Learn more about how to use
-  Kaboom.js by watching some of my tutorials <a href="https://youtube.com/@jslegenddev" target="_blank">here</a>.`,
-  library: `There are a lot of programming books on my shelves. There is even one in French (I also speak French btw).
-  I probably only read one of them. Who else compulsively buys technical books without ever finishing them?`,
-  exit: `If you want to exit JSLegendDev's portfolio, just close the tab.`,
+  Text is rendered with HTML/CSS. So the textbox you're currently reading is not rendered within canvas.`,
+  library: `There are a lot of video games on my shelves.
+  I probably only play one of them. Who else compulsively buys games without ever finishing them?`,
+  exit: `If you want to exit my portfolio, just close the tab.`,
 };
