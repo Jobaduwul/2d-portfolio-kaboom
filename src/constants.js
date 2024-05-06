6e2155c61a52e72ec69eebee98d05a29d7fe7489
Kazi Jawadul Islam Jishan <87614554+Jobaduwul@users.noreply.github.com>
2024-05-06 12:40:22 +0600

    updates to constants

--- src/constants.js
+++ src/constants.js
@@ -7,8 +7,8 @@ export const dialogueData = {
   "cs-degree": `This is my academics certificates. I hung it on the wall because I'm proud of it. School: SFX Greenherald Int'l School (2006 - 2020). University: Islamic University of Technology (2021 - present).`,
   "sofa-table": `That's my sofa. I like to relax here and watch YouTube. 
   I also love to watch videos on YouTube. (If you like the content)
-  You'll learn how I built this portfolio you're currently playing through<a href="https://youtube.com/@jslegenddev" target="_blank">here</a>!`,
-  tv: `That's my TV. I've been watching tech youtubers a lot recently`,
+  You'll learn how I built this portfolio you're currently playing through <a href="https://youtube.com/@jslegenddev" target="_blank">here</a>!`,
+  tv: `That's my TV. I've been watching tech youtubers a lot recently.`,
   bed: `This where I sleep. Great ideas comes when I'm lying on my bed. When an idea strikes, I often have to write it down or else I won't be able to sleep because my mental energy is consumed by it.`,
   resume: `This is my desk and on it is my resume. <a href="https://www.linkedin.com/in/jobaduwul/overlay/1711832834914/single-media-viewer/?profileId=ACoAADbAR8IB69YrdfKQ6pAJ9AmHSgUCE6XFKCQ" target="_blank">Check it out?</a>
   Contact me at kazijawadulislam123@gmail.com if you have any interesting job opportunities!`,
